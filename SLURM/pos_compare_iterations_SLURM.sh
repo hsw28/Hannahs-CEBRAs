302d9284ef1c5c7af38f7d328d5bcded49c09c5e
hsw28 <holdbackaglacier@yahoo.com>
2024-05-10 14:20:21 -0500

    trying to get slurm emails

--- SLURM/pos_compare_iterations_SLURM.sh
+++ SLURM/pos_compare_iterations_SLURM.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=AM_SLURM_out.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
-#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hsw@northwestern.edu  ## your email
 
 
