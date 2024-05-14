5fedcb57a6ed1df1d5ae8adeacb8e44adb8ffed9
hsw28 <holdbackaglacier@yahoo.com>
2024-05-14 16:44:03 -0500

    added second instance of compare iterations to run on slurm

---
+++ pos_compare_iterations2.py
@@ -0,0 +1,102 @@
+import sys
+sys.path.append('/home/hsw967/Programming/Hannahs-CEBRAs')
+sys.path.append('/home/hsw967/Programming/Hannahs-CEBRAs/scripts')
+sys.path.append('/Users/Hannah/Programming/Hannahs-CEBRAs')
+from torch.nn.modules import AdaptiveMaxPool2d
+from cebra import CEBRA
+import numpy as np
+import pandas as pd
+import torch
+import random
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import r2_score
+from scipy import stats
+from pos_score import pos_score
+from datetime import datetime
+from hold_out import hold_out
+
+
+#does not make figures
+#use to run a bunch of times
+# Example usage:
+# traceA1An_An, traceAnB1_An, traceA1An_A1, traceAnB1_B1, posAn, posA1, posB1 are to be defined or loaded before calling this function.
+# results = pos_compare_iterations(traceA1An_An, traceAnB1_An, traceA1An_A1, traceAnB1_B1, posAn, posA1, posB1)
+# print(results)
+
+
+
+def train_and_evaluate(cebra_model, trace_train, trace_test, test_trace, pos_train, pos_test, test_pos):
+    cebra_model.fit(trace_train, pos_train)
+    train_transformed = cebra_model.transform(trace_train)
+    test_transformed = cebra_model.transform(trace_test)
+    test_external_transformed = cebra_model.transform(test_trace)
+    return pos_score(train_transformed, test_transformed, pos_train, pos_test), pos_score(train_transformed, test_external_transformed, pos_train, test_pos)
+
+def generate_headers():
+    prefixes = ["A1An_held_out", "A1", "B1An_held_out", "B1", "SHUFF_A1An_held_out", "SHUFF_A1", "SHUFF_B1An_held_out", "SHUFF_B1"]
+    metrics = ["r2", "Knn_pos_err", "distance_mean", "distance_median"]
+    headers = []
+
+    for prefix in prefixes:
+        for metric in metrics:
+            headers.append(f"{prefix}_{metric}")
+
+    return ','.join(headers)
+
+def pos_compare_iterations2(traceA1An_An, traceAnB1_An, traceA1An_A1, traceAnB1_B1, posAn, posA1, posB1,iterations):
+
+    output_dimension = 3
+    learning_rate = 0.000775
+    min_temperature = 0.1
+    max_iterations = 18000
+    distance = 'cosine'
+
+    cebra_model = CEBRA(model_architecture='offset10-model',
+                        batch_size=512,
+                        learning_rate=learning_rate,
+                        temperature_mode='auto',
+                        min_temperature=min_temperature,
+                        output_dimension=output_dimension,
+                        max_iterations=max_iterations,
+                        distance=distance,
+                        conditional='time_delta',
+                        device='cuda_if_available',
+                        num_hidden_units=32,
+                        time_offsets=1,
+                        verbose=False)
+
+
+    results = np.zeros((iterations, 32))  # Each iteration results in 32 outputs
+
+    try:
+        for i in range(iterations):
+            traceA1An_An_train, traceA1An_An_test = hold_out(traceA1An_An, 75)
+            posAn_train, posAn_test = hold_out(posAn, 75)
+            traceAnB1_An_train, traceAnB1_An_test = hold_out(traceAnB1_An, 75)
+            posAnB1_train, posAnB1_test = hold_out(posAn, 75)
+
+            indices = np.random.permutation(posAn.shape[0])  # Get a permutation of the row indices
+            posAn_shuffled = posAn[indices, :] #apply and shuffle
+
+            posAn_train_shuffled, posAn_test_shuffled = hold_out(posAn_shuffled, 75)
+
+
+            regular_A1 = train_and_evaluate(cebra_model, traceA1An_An_train, traceA1An_An_test, traceA1An_A1, posAn_train, posAn_test, posA1)
+            shuffled_A1 = train_and_evaluate(cebra_model, traceA1An_An_train, traceA1An_An_test, traceA1An_A1, posAn_train_shuffled, posAn_test_shuffled, posA1)
+            regular_B1 = train_and_evaluate(cebra_model, traceAnB1_An_train, traceAnB1_An_test, traceAnB1_B1, posAn_train, posAn_test, posB1)
+            shuffled_B1 = train_and_evaluate(cebra_model, traceAnB1_An_train, traceAnB1_An_test, traceAnB1_B1, posAn_train, posAn_test_shuffled, posB1)
+
+            # Flatten results into a single row per iteration
+            results[i] = np.concatenate((np.ravel(regular_A1), np.ravel(regular_B1), np.ravel(shuffled_A1), np.ravel(shuffled_B1)))
+
+            print(results)
+
+        # Save the results to a CSV file with the current date and time in the filename
+        current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        filename = f"pos_compare_lr{learning_rate}_mt{min_temperature}_mi{max_iterations}_d{distance}_{current_time}.csv"
+        header = generate_headers()
+        np.savetxt(filename, results, delimiter=',', header=header, comments='', fmt='%.3f')  # Adjust precision as needed
+        print(f"Results saved to {filename}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return results
---
+++ scripts/pos_compare_iterations_script2.py
@@ -0,0 +1,102 @@
+import sys
+sys.path.append('/Users/Hannah/Programming/Hannahs-CEBRAs/')
+sys.path.append('/home/hsw967/Programming/Hannahs-CEBRAs')
+sys.path.append('/home/hsw967/Programming/Hannahs-CEBRAs/scripts')
+import argparse
+import cebra
+from cebra import CEBRA
+import cebra.helper as cebra_helper
+import numpy as np
+from pos_compare_iterations import pos_compare_iterations
+from smoothpos import smoothpos
+from ca_velocity import ca_velocity
+
+#for using with slurm to run over a bunch of iterations
+# python pos_compare_iterations_script.py traceA1An_An traceAnB1_An traceA1An_A1 traceAnB1_B1 PosAn PosA1 PosB1 --iterations 10
+# python /Users/Hannah/Programming/Hannahs-CEBRAs/scripts/pos_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./PosAn.mat ./PosA1.mat ./PosB1.mat --iterations 1
+
+# Setup argparse for command line arguments
+parser = argparse.ArgumentParser(description="Run decoding with CEBRA.")
+parser.add_argument("traceA1An_An", type=str, help="Path to the traceA1An_An data file.")
+parser.add_argument("traceAnB1_An", type=str, help="Path to the traceAnB1_An data file.")
+parser.add_argument("traceA1An_A1", type=str, help="Path to the traceA1An_A1 data file.")
+parser.add_argument("traceAnB1_B1", type=str, help="Path to the traceAnB1_B1 data file.")
+parser.add_argument("PosAn", type=str, help="Path to the PosAn data file.")
+parser.add_argument("PosA1", type=str, help="Path to the PosA1 data file.")
+parser.add_argument("PosB1", type=str, help="Path to the PosB1 data file.")
+parser.add_argument("--iterations", type=int, default=100, help="Number of iterations to run.")
+
+
+# Parse arguments
+args = parser.parse_args()
+
+traceA1An_An = cebra.load_data(file=args.traceA1An_An)  # Adjust 'your_key_here' as necessary
+traceAnB1_An = cebra.load_data(file=args.traceAnB1_An)  # Adjust 'your_key_here' as necessary
+traceA1An_A1 = cebra.load_data(file=args.traceA1An_A1)  # Adjust 'your_key_here' as necessary
+traceAnB1_B1 = cebra.load_data(file=args.traceAnB1_B1)  # Adjust 'your_key_here' as necessary
+PosAn = cebra.load_data(file=args.PosAn)  # Adjust 'your_key_here' as necessary
+PosA1 = cebra.load_data(file=args.PosA1)  # Adjust 'your_key_here' as necessary
+PosB1 = cebra.load_data(file=args.PosB1)  # Adjust 'your_key_here' as necessary
+
+traceA1An_An = np.transpose(traceA1An_An)
+traceAnB1_An = np.transpose(traceAnB1_An)
+traceA1An_A1 = np.transpose(traceA1An_A1)
+traceAnB1_B1 = np.transpose(traceAnB1_B1)
+
+PosA1 = smoothpos(PosA1)
+PosAn = smoothpos(PosAn)
+PosB1 = smoothpos(PosB1)
+
+#get every other point and check length
+PosA1 = PosA1[:,1:]
+PosA1 = PosA1[::2]
+if len(PosA1) > len(traceA1An_A1):
+    PosA1 = PosA1[:len(traceA1An_A1)]
+
+
+PosAn = PosAn[:,1:]
+PosAn = PosAn[::2]
+if len(PosAn) > len(traceA1An_An):
+    PosAn = PosAn[:len(traceA1An_An)]
+
+PosB1 = PosB1[:,1:]
+PosB1 = PosB1[::2]
+if len(PosB1) > len(traceAnB1_B1):
+    PosB1 = PosB1[:len(traceAnB1_B1)]
+
+
+
+vel_A1 = ca_velocity(PosA1)
+vel_An = ca_velocity(PosAn)
+vel_B1 = ca_velocity(PosB1)
+
+print(PosA1.shape)
+print(vel_A1.shape)
+
+
+
+high_vel_indices_A1 = np.where(vel_A1 >= 4)[0]
+high_vel_indices_An = np.where(vel_An >= 4)[0]
+high_vel_indices_B1 = np.where(vel_B1 >= 4)[0]
+
+if high_vel_indices_A1.size > 0 and high_vel_indices_A1[-1] + 1 < len(PosA1):
+    high_vel_indices_A1 = high_vel_indices_A1 + 1
+if high_vel_indices_An.size > 0 and high_vel_indices_An[-1] + 1 < len(PosAn):
+    high_vel_indices_An = high_vel_indices_An + 1
+if high_vel_indices_B1.size > 0 and high_vel_indices_B1[-1] + 1 < len(PosB1):
+    high_vel_indices_B1 = high_vel_indices_B1 + 1
+
+PosA1 = PosA1[high_vel_indices_A1]
+PosAn = PosAn[high_vel_indices_An]
+PosB1 = PosB1[high_vel_indices_B1]
+
+traceA1An_A1 = traceA1An_A1[high_vel_indices_A1]
+traceA1An_An = traceA1An_An[high_vel_indices_An]
+traceAnB1_An = traceAnB1_An[high_vel_indices_An]
+traceAnB1_B1 = traceAnB1_B1[high_vel_indices_B1]
+
+
+
+
+
+pos_compare_iterations2(traceA1An_An, traceAnB1_An, traceA1An_A1, traceAnB1_B1, PosAn, PosA1, PosB1, args.iterations)
