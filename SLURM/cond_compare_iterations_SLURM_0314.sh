7e44003eced572c711c679c2400d36678d5abd24
hsw28 <holdbackaglacier@yahoo.com>
2024-05-29 19:43:07 -0500

    changed memory requirements

--- SLURM/cond_compare_iterations_SLURM_0314.sh
+++ SLURM/cond_compare_iterations_SLURM_0314.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-4 ## number of jobs to run "in parallel"
-#SBATCH --mem=220GB
+#SBATCH --array=0-3 ## number of jobs to run "in parallel"
+#SBATCH --mem=90GB
 #SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=AM_SLURM_out.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
@@ -26,4 +26,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 # Run the Python script with hardcoded arguments as sbatch ~/Programming/Hannahs-CEBRAs/SLURM/pos_compare_iterations_SLURM.sh
 #python /Users/Hannah/Programming/Hannahs-CEBRAs/scripts/pos_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./PosAn.mat ./PosA1.mat ./PosB1.mat --iterations 10 --parameter_set_name set0222
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 2 0 --iterations 100 --parameter_set_name set0314
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 2 0 --iterations 125 --parameter_set_name set0314
