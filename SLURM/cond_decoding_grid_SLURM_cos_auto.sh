3c8b579e63c11c84db6b2d0e38a29ba7341d2def
hsw28 <holdbackaglacier@yahoo.com>
2024-06-19 17:58:07 -0500

    still working on 0222 cond

--- SLURM/cond_decoding_grid_SLURM_cos_auto.sh
+++ SLURM/cond_decoding_grid_SLURM_cos_auto.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0 ## number of jobs to run "in parallel"
-#SBATCH --mem=80GB
+#SBATCH --mem=30GB
 #SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=AM_SLURM_out.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
@@ -24,4 +24,6 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 # Run the Python script with hardcoded arguments as sbatch ~/Programming/Hannahs-CEBRAs/SLURM/pos_compare_iterations_SLURM.sh
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid4.py ./traceAnB1_An.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkB1.mat 2 0 --learning_rate 0.05,0.02,0.0095,.0035,.0002 --min_temperature 0.3,0.66,1,1.33,1.67,2,2.33,2.66,3,3.5 --max_iterations 8000,10000,13000,16000,18000,20000,25000
+#python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid_cos_auto.py ./traceAnB1_An.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkB1.mat 2 0 --learning_rate 0.05,0.02,0.0095,.0035,.0002 --min_temperature 0.3,0.66,1,1.33,1.67,2,2.33,2.66,3,3.5 --max_iterations 8000,10000,13000,16000,18000,20000,25000
+
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid_cos_auto.py ./traceA1An_An.mat ./traceA1An_A1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat 2 0 --learning_rate 0.0035,0.0075,0.0095,0.02,0.05 --min_temperature 1,2,2.33,2.66,3,3.5 --max_iterations 8000,9000,10000,13000,16000,25000
--- SLURM/cond_decoding_grid_SLURM_euc_auto.sh
+++ SLURM/cond_decoding_grid_SLURM_euc_auto.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0 ## number of jobs to run "in parallel"
-#SBATCH --mem=80GB
+#SBATCH --mem=40GB
 #SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=AM_SLURM_out.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
@@ -24,4 +24,6 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 # Run the Python script with hardcoded arguments as sbatch ~/Programming/Hannahs-CEBRAs/SLURM/pos_compare_iterations_SLURM.sh
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid.py ./traceAnB1_An.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkB1.mat 2 0 --learning_rate 0.01,0.0075,0.0055,0.003,0.001,0.00075 --min_temperature .33,.66,1,1.33,1.67,2,2.33 --max_iterations 8000,13000,16000,20000,2500025000
+#python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid_euc_auto.py ./traceAnB1_An.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkB1.mat 2 0 --learning_rate 0.05,0.02,0.0095,.0055,.0035,.0002 --min_temperature 0.3,0.66,1,1.33,1.67,2,2.33,2.66,3,3.5 --max_iterations 9000,10000,13000,16000,18000,20000,25000
+
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_decoding_AvsB_grid_euc_auto.py ./traceA1An_An.mat ./traceA1An_A1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat 2 0 --learning_rate 0.0035,0.0075,0.0095,0.02,0.05 --min_temperature 1.33,2,2.33,2.66,3,3.5 --max_iterations 6000,7500,8000,10000,18000,20000,25000
