de8a84d0c256db5ef391b38b248929a26403e0d2
hsw28 <holdbackaglacier@yahoo.com>
2024-07-01 15:35:34 -0500

    pos decoding 0313.....

--- SLURM/pos_decoding_grid_cosine_SLURM.sh
+++ SLURM/pos_decoding_grid_cosine_SLURM.sh
@@ -24,4 +24,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 # Run the Python script with hardcoded arguments as sbatch ~/Programming/Hannahs-CEBRAs/SLURM/pos_compare_iterations_SLURM.sh
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/pos_decoding_AvsB_grid.py ./traceA1An_An.mat ./traceA1An_A1.mat ./posAn.mat ./posA1.mat --learning_rate .0085,0.000325,0.00055 --min_temperature 0.004525,0.008 --max_iterations 12000,16000,20000,24000
+python /Users/Hannah/Programming/Hannahs-CEBRAs/scripts/pos_decoding_AvsB_grid.py ./traceA1An_An.mat ./traceA1An_A1.mat ./posAn.mat ./posA1.mat --learning_rate 0.00055,0.0001,0.0000005,0.00000025,0.0000001,0.00000004,0.000000025,0.00000001,0.000000055,0.000000001,0.0000000005 --min_temperature .000000001,.1,.75,1 --max_iterations 20000,25000,30000,35000,40000
