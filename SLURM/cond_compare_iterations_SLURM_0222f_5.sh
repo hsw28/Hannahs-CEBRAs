d284475e4b353047e353eb2df00c4ae88f5f7ddb
hsw28 <holdbackaglacier@yahoo.com>
2024-07-17 13:31:13 -0500

    trying 0222 cond again

--- SLURM/cond_compare_iterations_SLURM_0222f_5.sh
+++ SLURM/cond_compare_iterations_SLURM_0222f_5.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-4 ## number of jobs to run "in parallel"
+#SBATCH --array=0-9 ## number of jobs to run "in parallel"
 #SBATCH --mem=18GB
 #SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
@@ -24,4 +24,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 
 # Run the Python script with hardcoded arguments as sbatch ~/Programming/Hannahs-CEBRAs/SLURM/pos_compare_iterations_SLURM.sh
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 100 --parameter_set_name set0222f
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_compare_iterations_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 50 --parameter_set_name set0222f
