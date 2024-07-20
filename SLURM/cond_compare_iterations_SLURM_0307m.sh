cfa82ef30085143c131ec00d1a149eed91e480eb
hsw28 <holdbackaglacier@yahoo.com>
2024-07-20 12:20:27 -0500

    0307 cond

--- SLURM/cond_compare_iterations_SLURM_0307m.sh
+++ SLURM/cond_compare_iterations_SLURM_0307m.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --array=0-4 ## number of jobs to run "in parallel"
 #SBATCH --mem=18GB
-#SBATCH --time=36:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=AM_SLURM_out.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=BEGIN,END,FAIL
