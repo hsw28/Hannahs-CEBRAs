7cfcfb0e16533e793daf4fa34aee31a108091c3f
hsw28 <holdbackaglacier@yahoo.com>
2026-05-15 11:12:30 -0500

    0307b

--- scripts/cond_compare_iterations_script.py
+++ scripts/cond_compare_iterations_script.py
@@ -35,7 +35,7 @@ parameter_sets = parameter_sets = {
 
     "set0307": {"learning_rate": 0.0025, "min_temperature": 4, "max_iterations": 70000, "distance": "euclidean", "temp_mode": "constant"},
 
-    "set0307b": {"learning_rate": 0.0035, "min_temperature": 4, "max_iterations": 50000, "distance": 'euclidean', "temp_mode": 'auto'},
+    "set0307b": {"learning_rate": 0.0025, "min_temperature": 4.25, "max_iterations": 63000, "distance": 'cosine', "temp_mode": 'constant'},
 
     "set0307c": {"learning_rate": 0.0025, "min_temperature": 4, "max_iterations": 70000, "distance": 'euclidean', "temp_mode": 'constant'}, #yes
 
