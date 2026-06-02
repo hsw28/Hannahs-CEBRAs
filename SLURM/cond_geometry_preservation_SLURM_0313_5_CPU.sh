bb9d0c1e7bdc776f7096a4c1f9cfdade3acb6c84
hsw28 <holdbackaglacier@yahoo.com>
2026-06-01 20:59:32 -0500

    not just A in cpu

--- SLURM/cond_geometry_preservation_SLURM_0313_5_CPU.sh
+++ SLURM/cond_geometry_preservation_SLURM_0313_5_CPU.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=20GB
-#SBATCH --time=15:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name="geom_0313_${SLURM_ARRAY_TASK_ID}"
 #SBATCH --output=geometry_preservation_0313.%A_%a.out
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -20,4 +20,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 #python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 1 --parameter_set_name set0313 --rat_id rat0313
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --save_branch A --parameter_set_name set0313 --rat_id rat0313
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --parameter_set_name set0313 --rat_id rat0313
