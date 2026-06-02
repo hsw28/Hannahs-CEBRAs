350b82d6cfdd4e26eab3af2fc84549a7e16c531a
hsw28 <holdbackaglacier@yahoo.com>
2026-06-02 12:12:08 -0500

    back to gpu cond prez

--- SLURM/cond_geometry_preservation_SLURM_0313_5.sh
+++ SLURM/cond_geometry_preservation_SLURM_0313_5.sh
@@ -22,4 +22,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts
 
 #python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 1 --parameter_set_name set0313 --rat_id rat0313
 
-python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --save_branch A --parameter_set_name set0313 --rat_id rat0313
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --parameter_set_name set0313 --rat_id rat0313
---
+++ SLURM/cond_geometry_preservation_SLURM_0816_5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=p32072
+#SBATCH --partition=gengpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=0 ## number of jobs to run "in parallel"
+#SBATCH --mem=12GB
+#SBATCH --time=3:00:00
+#SBATCH --job-name="geom_0816_${SLURM_ARRAY_TASK_ID}"
+#SBATCH --output=geometry_preservation_0816.%A_%a.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hsw@northwestern.edu
+
+module purge
+eval "$(conda shell.bash hook)"
+source activate ratinabox
+
+export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs"
+export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs/scripts"
+
+#python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 1 --parameter_set_name set0816 --rat_id rat0816
+
+#python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --save_branch A --parameter_set_name set0816 --rat_id rat0816
+python /home/hsw967/Programming/Hannahs-CEBRAs/scripts/cond_geometry_preservation_script.py ./traceA1An_An.mat ./traceAnB1_An.mat ./traceA1An_A1.mat ./traceAnB1_B1.mat ./eyeblinkAn.mat ./eyeblinkA1.mat ./eyeblinkB1.mat 5 0 --iterations 20 --shuffles 0 --parameter_set_name set0816 --rat_id rat0816
